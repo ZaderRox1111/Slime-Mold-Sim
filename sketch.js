557495262d46a0d2a83d3c4e8a8a41e930a8b82e
Zachariah Derrick <72170300+ZaderRox1111@users.noreply.github.com>
2021-05-01 12:46:48 -0700

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -2,122 +2,10 @@
 var gravity = 0;
 
 var NUMBER_OF_PARTICLES = 10;
+var EVAPORATION_RATE = 0;
 
 var WIN_WIDTH = 600;
 var WIN_HEIGHT = 600;
-
-//particle function
-function Particle(x, y, radius, tailLength)
-{
-    this.x = x;
-    this.y = y;
-    this.radius = radius;
-    this.tailLength = tailLength;
-    
-    this.ySpeed = 0;
-    this.xSpeed = 0;
-    
-    this.history = [];
-    
-    var locationVector;
-    
-    //create an update function
-    this.update = function()
-    {
-        //update position and velocity
-        this.updatePosSpeed();
-        
-        //check collisions with walls
-        this.checkForCollisions();
-        
-        //create and manage the tail
-        this.updateTail();
-    };
-    
-    //create a showing function to draw the particle
-    this.show = function()
-    {
-        //variables
-        var index;
-        
-        //draws every location in the history
-        for (index = 0; index < this.history.length; index++)
-        {
-            //get the x and y values from the history
-            var xHist = this.history[index][0];
-            var yHist = this.history[index][1];
-            
-            //change the point size with the spot in history
-            var histRadius = this.radius * (index / this.history.length);
-            
-            //draw an ellipse at the history point
-            noStroke();
-            fill(200, 200, 200);
-            ellipse(xHist, yHist, histRadius, histRadius);
-        }
-        
-        //draw the main ellipse
-        fill(97, 97, 97);
-        ellipse(this.x, this.y, this.radius, this.radius);
-    };
-
-    this.checkForCollisions = function() 
-    {
-    	//check collisions with border
-    	if (this.y > WIN_HEIGHT)
-        {
-            //so it never is caught in an infinite loop
-            this.y = WIN_HEIGHT;
-            this.ySpeed = -0.9 * this.ySpeed;
-        }
-        else if (this.y < 0)
-        {
-            this.y = 0;
-            this.ySpeed = -0.9 * this.ySpeed;
-        }
-        if (this.x > WIN_WIDTH)
-        {
-            //so it never is caught in an infinite loop
-            this.x = WIN_WIDTH;
-            this.xSpeed = -0.9 * this.xSpeed;
-        }
-        else if (this.x < 0)
-        {
-            this.x = 0;
-            this.xSpeed = -0.9 * this.xSpeed;
-        }
-    };
-
-    this.updatePosSpeed = function() 
-    {
-    	//update position then speed
-        this.x += this.xSpeed;
-    	this.y += this.ySpeed;
-        this.xSpeed += Math.cos(this.x * 25);
-        this.ySpeed += gravity + Math.sin(this.y * 25);
-    };
-
-    this.updateTail = function()
-    {
-    	//put the current location into the history
-        locationVector = [this.x, this.y];
-        this.history.push(locationVector);
-        
-        //if the history is longer than tail length, splice it off
-        if (this.history.length > this.tailLength)
-        {
-            this.history.splice(0, 1);   
-        }
-    };
-
-    //goes over every pixel on screen and uses a box blur to blur it
-    this.blurScreen = function()
-    {
-    	var black = color(0);
-    	set(30, 20, black);
-    	updatePixels();
-    };
-}
 
 function PixelParticle(x, y, direction)
 {
@@ -141,8 +29,6 @@ function PixelParticle(x, y, direction)
 
 		//move the particle
 		this.move();
-
-		//blur the particles
 	};
 
 	//draws the particle at the location
@@ -192,7 +78,7 @@ function makeMultiplePixelParticles(numberParticles)
 	//fill the array with particles
 	for(index = 0; index < numberParticles; index++)
 	{
-		particlesArray[index] = new PixelParticle(500, 500, Math.random());
+		particlesArray[index] = new PixelParticle(300, 300, Math.PI * 2 * Math.random());
 	}
 
 	//return the array
@@ -214,13 +100,17 @@ function updateAllParticles(particlesArray, numberParticles)
 	updatePixels();
 }
 
-var particle1 = new Particle(100, 100, 15, 25);
+
+
+
 var particle2 = new PixelParticle(500, 500, 1);
 
 particlesArray = makeMultiplePixelParticles(NUMBER_OF_PARTICLES);
 
 function setup() {
   	// put setup code here
+    //set pixel density to one
+    pixelDensity(1);
   	var canvas = createCanvas(WIN_WIDTH, WIN_HEIGHT);
 
   	//put the sketch under an html id
@@ -228,16 +118,8 @@ function setup() {
 }
 
 function draw() {
-/*  	// put drawing code here
-  	//draw background first
-  	background(220);
-
-  	//draw and update particle
-  	particle1.show();
-    particle1.update();
-*/
-
 	//draw and update the particle
-	particle2.update();
+    background(10);
+	//particle2.update();
 	updateAllParticles(particlesArray, NUMBER_OF_PARTICLES);
 }
